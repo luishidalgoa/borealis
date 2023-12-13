735b8b110ab66faf616d1403a895868ea8701532
Daniil Vinogradov <xitrix@bk.ru>
2023-12-13 02:03:59 +0100

    MetalANGLE iOS support

--- library/lib/platforms/sdl/sdl_platform.cpp
+++ library/lib/platforms/sdl/sdl_platform.cpp
@@ -83,7 +83,8 @@ SDLPlatform::SDLPlatform()
                 { "zh_TW", LOCALE_ZH_HANT },
                 { "ja_JP", LOCALE_JA },
                 { "ko_KR", LOCALE_Ko },
-                { "it_IT", LOCALE_IT }
+                { "it_IT", LOCALE_IT },
+                { "ru", LOCALE_RU }
             };
             std::string lang = std::string { locales->language };
             if (locales->country)
