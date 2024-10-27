c05d48ff295677dc2ba5db2be09ca48e7939bd6c
Daniil Vinogradov <xitrix@bk.ru>
2024-10-27 11:13:27 +0100

    Rumble iOS device if Player1 controller does not support it

--- library/lib/platforms/desktop/ios_darwin.mm
+++ library/lib/platforms/desktop/ios_darwin.mm
@@ -2,6 +2,175 @@
 #import <borealis/platforms/desktop/desktop_platform.hpp>
 #import <UIKit/UIKit.h>
 
+#import <CoreHaptics/CoreHaptics.h>
+#import <GameController/GameController.h>
+
+@interface HapticContext : NSObject
+
+-(void)setMotorAmplitude:(unsigned short)amplitude;
+-(void)cleanup;
+
++(HapticContext*) createContextForHighFreqMotor;
++(HapticContext*) createContextForLowFreqMotor;
++(HapticContext*) createContextForLeftTrigger;
++(HapticContext*) createContextForRightTrigger;
+
+@end
+
+@implementation HapticContext {
+    GCControllerPlayerIndex _playerIndex;
+    CHHapticEngine* _hapticEngine API_AVAILABLE(ios(13.0), tvos(14.0));
+    id<CHHapticPatternPlayer> _hapticPlayer API_AVAILABLE(ios(13.0), tvos(14.0));
+    BOOL _playing;
+}
+
+-(void)cleanup API_AVAILABLE(ios(14.0), tvos(14.0)) {
+    if (_hapticPlayer != nil) {
+        [_hapticPlayer cancelAndReturnError:nil];
+        _hapticPlayer = nil;
+    }
+    if (_hapticEngine != nil) {
+        [_hapticEngine stopWithCompletionHandler:nil];
+        _hapticEngine = nil;
+    }
+}
+
+-(void)setMotorAmplitude:(unsigned short)amplitude API_AVAILABLE(ios(14.0), tvos(14.0)) {
+    NSError* error;
+
+    // Check if the haptic engine died
+    if (_hapticEngine == nil) {
+        return;
+    }
+
+    // Stop the effect entirely if the amplitude is 0
+    if (amplitude == 0) {
+        if (_playing) {
+            [_hapticPlayer stopAtTime:0 error:&error];
+            _playing = NO;
+        }
+
+        return;
+    }
+
+    if (_hapticPlayer == nil) {
+        // We must initialize the intensity to 1.0f because the dynamic parameters are multiplied by this value before being applied
+        CHHapticEventParameter* intensityParameter = [[CHHapticEventParameter alloc] initWithParameterID:CHHapticEventParameterIDHapticIntensity value:1.0f];
+        CHHapticEvent* hapticEvent = [[CHHapticEvent alloc] initWithEventType:CHHapticEventTypeHapticContinuous parameters:[NSArray arrayWithObject:intensityParameter] relativeTime:0 duration:GCHapticDurationInfinite];
+        CHHapticPattern* hapticPattern = [[CHHapticPattern alloc] initWithEvents:[NSArray arrayWithObject:hapticEvent] parameters:[[NSArray alloc] init] error:&error];
+        if (error != nil) {
+            NSLog(@"Controller %d: Haptic pattern creation failed: %@", _playerIndex, error);
+            return;
+        }
+
+        _hapticPlayer = [_hapticEngine createPlayerWithPattern:hapticPattern error:&error];
+        if (error != nil) {
+            NSLog(@"Controller %d: Haptic player creation failed: %@", _playerIndex, error);
+            return;
+        }
+    }
+
+    CHHapticDynamicParameter* intensityParameter = [[CHHapticDynamicParameter alloc] initWithParameterID:CHHapticDynamicParameterIDHapticIntensityControl value:amplitude / 65535.0f relativeTime:0];
+    [_hapticPlayer sendParameters:[NSArray arrayWithObject:intensityParameter] atTime:CHHapticTimeImmediate error:&error];
+    if (error != nil) {
+        NSLog(@"Controller %d: Haptic player parameter update failed: %@", _playerIndex, error);
+        return;
+    }
+
+    if (!_playing) {
+        [_hapticPlayer startAtTime:0 error:&error];
+        if (error != nil) {
+            _hapticPlayer = nil;
+            NSLog(@"Controller %d: Haptic playback start failed: %@", _playerIndex, error);
+            return;
+        }
+
+        _playing = YES;
+    }
+}
+
+-(id) initWithLocality:(GCHapticsLocality)locality API_AVAILABLE(ios(14.0), tvos(14.0)) {
+    NSLog(@"Controller %d does not support haptic locality: %@", 0, locality);
+    if (@available(iOS 13.0, *)) {
+        NSError *error = nil;
+        _hapticEngine = [[CHHapticEngine alloc] initAndReturnError:&error];
+    } else{
+        return nil;
+    }
+    _playerIndex = GCControllerPlayerIndex1;
+
+    NSError* error;
+    [_hapticEngine startAndReturnError:&error];
+    if (error != nil) {
+        NSLog(@"Controller %d: Haptic engine failed to start: %@", _playerIndex, error);
+        return nil;
+    }
+
+    __weak typeof(self) weakSelf = self;
+    _hapticEngine.stoppedHandler = ^(CHHapticEngineStoppedReason stoppedReason) {
+        HapticContext* me = weakSelf;
+        if (me == nil) {
+            return;
+        }
+
+        NSLog(@"Controller %d: Haptic engine stopped: %p", me->_playerIndex, stoppedReason);
+        me->_hapticPlayer = nil;
+        me->_hapticEngine = nil;
+        me->_playing = NO;
+    };
+    _hapticEngine.resetHandler = ^{
+        HapticContext* me = weakSelf;
+        if (me == nil) {
+            return;
+        }
+
+        NSLog(@"Controller %d: Haptic engine reset", me->_playerIndex);
+        me->_hapticPlayer = nil;
+        me->_playing = NO;
+        [me->_hapticEngine startAndReturnError:nil];
+    };
+
+    return self;
+}
+
++(HapticContext*) createContextForHighFreqMotor {
+    if (@available(iOS 14.0, tvOS 14.0, *)) {
+        return [[HapticContext alloc] initWithLocality:GCHapticsLocalityRightHandle];
+    }
+    else {
+        return nil;
+    }
+}
+
++(HapticContext*) createContextForLowFreqMotor {
+    if (@available(iOS 14.0, tvOS 14.0, *)) {
+        return [[HapticContext alloc] initWithLocality:GCHapticsLocalityLeftHandle];
+    }
+    else {
+        return nil;
+    }
+}
+
++(HapticContext*) createContextForLeftTrigger {
+    if (@available(iOS 14.0, tvOS 14.0, *)) {
+        return [[HapticContext alloc] initWithLocality:GCHapticsLocalityLeftTrigger];
+    }
+    else {
+        return nil;
+    }
+}
+
++(HapticContext*) createContextForRightTrigger {
+    if (@available(iOS 14.0, tvOS 14.0, *)) {
+        return [[HapticContext alloc] initWithLocality:GCHapticsLocalityRightTrigger];
+    }
+    else {
+        return nil;
+    }
+}
+
+@end
+
 namespace brls {
 
 ThemeVariant ios_theme() {
@@ -33,4 +202,22 @@ float ios_battery() {
     return 0;
 #endif
 }
+
+HapticContext* contextForHighFreqMotor = NULL;
+HapticContext* contextForLowFreqMotor = NULL;
+
+void init_device_rumble() {
+    if (contextForHighFreqMotor == NULL)
+        contextForHighFreqMotor = [HapticContext createContextForLowFreqMotor];
+
+    if (contextForLowFreqMotor == NULL)
+        contextForLowFreqMotor = [HapticContext createContextForHighFreqMotor];
+}
+
+void device_rumble(unsigned short lowFreqMotor, unsigned short highFreqMotor) {
+    init_device_rumble();
+    [contextForLowFreqMotor setMotorAmplitude:lowFreqMotor];
+    [contextForHighFreqMotor setMotorAmplitude:highFreqMotor];
+}
+
 };
