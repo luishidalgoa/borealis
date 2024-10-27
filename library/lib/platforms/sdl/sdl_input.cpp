b6c6e2c33eff7ef0e4d368929aa069517d14eb63
Daniil Vinogradov <xitrix@bk.ru>
2024-10-27 11:13:27 +0100

    Rumble iOS device if Player1 controller does not support it

--- library/lib/platforms/sdl/sdl_input.cpp
+++ library/lib/platforms/sdl/sdl_input.cpp
@@ -22,6 +22,14 @@
 namespace brls
 {
 
+#if defined(PLATFORM_IOS)
+extern void init_device_rumble();
+extern void device_rumble(unsigned short lowFreqMotor, unsigned short highFreqMotor);
+#else
+void init_device_rumble() {}
+void device_rumble(unsigned short lowFreqMotor, unsigned short highFreqMotor) {}
+#endif
+
 #define SDL_GAMEPAD_BUTTON_NONE SIZE_MAX
 #define SDL_GAMEPAD_BUTTON_MAX 15
 #define SDL_GAMEPAD_AXIS_MAX 6
@@ -453,6 +461,8 @@ SDLInputManager::SDLInputManager(SDL_Window* window)
 
         pointerOffset.x = 0;
         pointerOffset.y = 0; });
+
+    init_device_rumble();
 }
 
 SDLInputManager::~SDLInputManager()
@@ -625,6 +635,11 @@ void SDLInputManager::sendRumble(unsigned short controller, unsigned short lowFr
     if (controllers.size() <= controller) return;
     SDL_GameController* c = controllers[controller].second;
 
+    if (!SDL_GameControllerHasRumble(c)) {
+        device_rumble(lowFreqMotor, highFreqMotor);
+        return;
+    }
+
     SDL_GameControllerRumble(c, lowFreqMotor, highFreqMotor, 30000);
 }
 
@@ -633,6 +648,11 @@ void SDLInputManager::sendRumble(unsigned short controller, unsigned short lowFr
     if (controllers.find(controller) == controllers.end())
         return;
     SDL_GameController* c = controllers[controller];
+
+    if (!SDL_GameControllerHasRumble(c)) {
+        device_rumble(lowFreqMotor, highFreqMotor);
+        return;
+    }
 
     SDL_GameControllerRumble(c, lowFreqMotor, highFreqMotor, 30000);
     SDL_GameControllerRumbleTriggers(c, leftTriggerFreqMotor, rightTriggerFreqMotor, 30000);
