d11d48140e68760d75001db74686fd5089184e27
xfangfang <2553041586@qq.com>
2025-02-19 16:40:03 +0800

    Allow custom swap interval (#96)
    * Allow custom swap interval
    
    * Fix build error
    
    * Update readme

--- library/include/borealis/core/application.hpp
+++ library/include/borealis/core/application.hpp
@@ -208,7 +208,48 @@ class Application
     static void setFPSStatus(bool enabled);
     static bool getFPSStatus();
     static size_t getFPS();
+
+    /**
+     * Set the FPS limit
+     * @param fps 0 to disable limit
+     */
     static void setLimitedFPS(size_t fps);
+
+    /**
+     * Set the swap interval
+     * Must be called after createWindow. On some platforms, it is impossible to set it back
+     * to 0 after setting it to a non-zero values, so you can use
+     * `VideoContext::swapInterval = 0;` (#include "borealis/core/video.hpp") before creating the window.
+     *
+     * Test results:
+     *
+     * ## Platforms that always vsync (interval: 1):
+     * iOS18/GLES3/SDL
+     * PS4/GLES2/SDL
+     *
+     * ## Platforms that only support enable and disable vsync (interval: 0,1):
+     * Android9/GLES2/SDL
+     * macOS13.0+/GL3.2/GLFW (will fixed to 120fps with vsync enabled, https://github.com/glfw/glfw/pull/2277)
+     * Windows11/GL3.2/GLFW
+     * Linux Wayland/GL3.2/GLFW
+     *
+     * ## Platforms that not support disable vsync (interval: 1,2,3,4):
+     * Windows11/D3D11/GLFW
+     * Switch/DEKO3D
+     *
+     * ## Platforms that support arbitrary values (interval: 0,1,2,3,4):
+     * PsVita/GXM
+     * PsVita/GLES2/SDL
+     * macOS13.0+/GL3.2/SDL
+     * Linux X11/GL3.2/GLFW
+     * Linux X11/GL3.2/SDL
+     * Linux Wayland/GL3.2/SDL
+     * Switch/GL4.3/GLFW
+     * Switch/GL4.3/SDL
+     *
+     * @param interval 0 to disable vsync
+     */
+    static void setSwapInterval(int interval);
 
     /**
      * If the value is set to true, the program will limit FPS to Application::DeactivatedFPS
--- library/lib/core/application.cpp
+++ library/lib/core/application.cpp
@@ -767,6 +767,11 @@ void Application::setLimitedFPS(size_t fps)
     Application::limitedFrameTime = fps == 0 ? 0 : 1000000.0f / fps;
 }
 
+void Application::setSwapInterval(int interval)
+{
+    Application::platform->getVideoContext()->setSwapInterval(interval);
+}
+
 void Application::notify(const std::string& text)
 {
     Application::notificationManager->notify(text);
